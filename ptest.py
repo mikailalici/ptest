a6c1e309bfa4a98badc50d888d51ccf7b3c50b36
Mikail Alici <mikailalici87@gmail.com>
2020-05-13 03:05:43 +0300

    ptest.py

---
+++ ptest.py
@@ -0,0 +1,35 @@
+import pytest
+import src
+from src import constraints
+from src import genetic
+import unittest
+
+UnitTestID=1
+Action=test_get_example
+
+Result = test_get_example_is_valid(example}:  #Action
+
+self.assertFalse(result)  #assert
+
+def test_get_example(): #testing get example
+    c = constraints.Constraint(';../src/alloy.txt')
+    v = c.get_example() #from constraints class
+    print(v)
+
+integrationID=1  #integID:1, Action is integratation constraints, genetic
+Action=constraintsandgenetic
+
+Result = constraints.Constraint_is_valid(test_integration1):
+
+self.assertFalse(result)  #assert
+
+def test_integration1():
+
+    c = constraints.Constraint(';../src/alloy.txt')
+    n = c.get_ndim()
+    s = 10
+    ga = genetic.Genetic(s,n,'../src/alloy.txt')
+    p = ga.get_population()
+    print (p)
+
+
